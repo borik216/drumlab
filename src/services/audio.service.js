0313c0ac0854762ec88f1fe2b930047c5e321c23
Boris Ryjkov <ryjkovboris@gmail.com>
2023-11-25 19:39:58 +0200

    trying to fix audio 1#

--- src/services/audio.service.js
+++ src/services/audio.service.js
@@ -27,7 +27,7 @@ async function setupSamples() {
 }
 
 function getPath(sound) {
-    return `/public/audio/${sound}.wav`
+    return path.join(process.cwd(),"public","assets","audio",`${sound}.wav`);
 }
 
 export function playSample(sound, volume = 1.0) {
